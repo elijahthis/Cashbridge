fc22199b033c922364f3a239c1ca03cb2360a98c
elijahthis <emperorelijah007@gmail.com>
2023-11-10 16:24:39 +0100

    feat: corrected header

--- src/component/header/HeaderOne.jsx
+++ src/component/header/HeaderOne.jsx
@@ -9,67 +9,80 @@ import ProfilePopup from "./ProfilePopup";
 import StorePopUp from "./StorePopUp";
 import ToggleBtn from "./ToggleBtn";
 import ModeToggler from "./ModeToggler";
+import { sidebarList } from "@/data/constants";
+import { usePathname } from "next/navigation";
+
 function HeaderOne({ handleSidebar }) {
-  const [popup, setPopup] = useState({
-    notification: false,
-    massage: false,
-    profile: false,
-    store: false,
-  });
+	const [popup, setPopup] = useState({
+		notification: false,
+		massage: false,
+		profile: false,
+		store: false,
+	});
 
-  const handlePopup = (name) => {
-    setPopup({ ...popup, [name]: !popup[name] });
-  };
+	const handlePopup = (name) => {
+		setPopup({ ...popup, [name]: !popup[name] });
+	};
 
-  return (
-    <header className="header-wrapper fixed z-30 hidden w-full md:block">
-      <div className="relative flex h-[108px] w-full items-center justify-between bg-white px-10 dark:bg-darkblack-600 2xl:px-[76px]">
-        <button
-          aria-label="none"
-          onClick={handleSidebar}
-          title="Ctrl+b"
-          type="button"
-          className="drawer-btn absolute left-0 top-auto rotate-180 transform"
-        >
-          <span>
-            <svg
-              width="16"
-              height="40"
-              viewBox="0 0 16 40"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0 10C0 4.47715 4.47715 0 10 0H16V40H10C4.47715 40 0 35.5228 0 30V10Z"
-                fill="#86272D"
-              />
-              <path
-                d="M10 15L6 20.0049L10 25.0098"
-                stroke="#ffffff"
-                strokeWidth="1.2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </span>
-        </button>
-        {/* page-title */}
-        <div>
-          <h3 className="text-xl font-bold text-bgray-900 dark:text-bgray-50 lg:text-3xl lg:leading-[36.4px]">
-            Dashboard
-          </h3>
-          <p className="text-xs font-medium text-bgray-600 dark:text-bgray-50 lg:text-sm lg:leading-[25.2px]">
-            Let’s check your update today
-          </p>
-        </div>
-        {/* search-bar */}
-        {/* <SearchBar /> */}
-        {/* quick access */}
-        <div className="quick-access-wrapper relative">
-          <div className="flex items-center space-x-[43px]">
-            <div className="hidden items-center space-x-5 xl:flex">
-              {/* <ModeToggler /> */}
-              {/* <ToggleBtn
+	const pathname = usePathname();
+
+	console.log("pathname", pathname);
+
+	return (
+		<header className="header-wrapper fixed z-30 hidden w-full md:block">
+			<div className="relative flex h-[108px] w-full items-center justify-between bg-white px-10 dark:bg-darkblack-600 2xl:px-[76px]">
+				<button
+					aria-label="none"
+					onClick={handleSidebar}
+					title="Ctrl+b"
+					type="button"
+					className="drawer-btn absolute left-0 top-auto rotate-180 transform"
+				>
+					<span>
+						<svg
+							width="16"
+							height="40"
+							viewBox="0 0 16 40"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M0 10C0 4.47715 4.47715 0 10 0H16V40H10C4.47715 40 0 35.5228 0 30V10Z"
+								fill="#86272D"
+							/>
+							<path
+								d="M10 15L6 20.0049L10 25.0098"
+								stroke="#ffffff"
+								strokeWidth="1.2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+						</svg>
+					</span>
+				</button>
+				{/* page-title */}
+				<div>
+					<h3 className="text-xl font-bold text-bgray-900 dark:text-bgray-50 lg:text-3xl lg:leading-[36.4px]">
+						{sidebarList
+							.filter(
+								(item) =>
+									(pathname.startsWith(item.link) && item.link !== "/") ||
+									(pathname === "/" && item.link === "/")
+							)[0]
+							?.title?.toUpperCase()}
+					</h3>
+					<p className="text-xs font-medium text-bgray-600 dark:text-bgray-50 lg:text-sm lg:leading-[25.2px]">
+						Let’s check your update today
+					</p>
+				</div>
+				{/* search-bar */}
+				{/* <SearchBar /> */}
+				{/* quick access */}
+				<div className="quick-access-wrapper relative">
+					<div className="flex items-center space-x-[43px]">
+						<div className="hidden items-center space-x-5 xl:flex">
+							{/* <ModeToggler /> */}
+							{/* <ToggleBtn
                 name="notification"
                 clickHandler={handlePopup}
                 icon={
@@ -85,7 +98,7 @@ function HeaderOne({ handleSidebar }) {
                   </svg>
                 }
               /> */}
-              {/* <ToggleBtn
+							{/* <ToggleBtn
                 active={true}
                 name="massage"
                 clickHandler={handlePopup}
@@ -126,27 +139,27 @@ function HeaderOne({ handleSidebar }) {
                   </svg>
                 }
               /> */}
-            </div>
-            <div className="hidden h-[48px] w-[1px] bg-bgray-300 dark:bg-darkblack-400 xl:block"></div>
-            {/* author */}
-            <Author showProfile={handlePopup} />
-          </div>
-          {/* notification ,message, store */}
-          <NotificationPopup
-            active={popup.notification}
-            handlePopup={handlePopup}
-          />
-          <MassagePopup active={popup.massage} handlePopup={handlePopup} />
-          <StorePopUp active={popup.store} handlePopup={handlePopup} />
-          <ProfilePopup active={popup.profile} handlePopup={handlePopup} />
-        </div>
-      </div>
-    </header>
-  );
+						</div>
+						<div className="hidden h-[48px] w-[1px] bg-bgray-300 dark:bg-darkblack-400 xl:block"></div>
+						{/* author */}
+						<Author showProfile={handlePopup} />
+					</div>
+					{/* notification ,message, store */}
+					<NotificationPopup
+						active={popup.notification}
+						handlePopup={handlePopup}
+					/>
+					<MassagePopup active={popup.massage} handlePopup={handlePopup} />
+					<StorePopUp active={popup.store} handlePopup={handlePopup} />
+					<ProfilePopup active={popup.profile} handlePopup={handlePopup} />
+				</div>
+			</div>
+		</header>
+	);
 }
 
 HeaderOne.propTypes = {
-  handleSidebar: ProtoTypes.func,
+	handleSidebar: ProtoTypes.func,
 };
 
 export default HeaderOne;
